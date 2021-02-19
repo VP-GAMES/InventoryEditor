84514be7cfd07ca51b3817fd75e6a671292fab17
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-02-19 11:41:55 +0100

    V 0.1.9 Add Item2D/3D as custom type for InventoryEditor

--- addons/inventory_editor/InventoryItem3D.gd
+++ addons/inventory_editor/InventoryItem3D.gd
@@ -5,12 +5,16 @@ extends Area
 class_name Item3D
 
 var inside
-var inventoryManager
+var _inventoryManager
 const InventoryManagerName = "InventoryManager"
+
+export(String) var inventory_uuid
+export(String) var item_uuid
+export(int) var quantity = 1
 
 func _ready() -> void:
 	if get_tree().get_root().has_node(InventoryManagerName):
-		inventoryManager = get_tree().get_root().get_node(InventoryManagerName)
+		_inventoryManager = get_tree().get_root().get_node(InventoryManagerName)
 	if not is_connected("body_entered", self, "_on_body_entered"):
 		assert(connect("body_entered", self, "_on_body_entered") == OK)
 	if not is_connected("body_exited", self, "_on_body_exited"):
@@ -18,7 +22,7 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	inside = true
-	# TODO here 
+	_inventoryManager.add_item(inventory_uuid, item_uuid, quantity)
 
 func _on_body_exited(body: Node) -> void:
 	inside = false

417f60542e57fdee961d4ff0af11a7960ce4d399
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-02-25 14:18:14 +0100

    V 0.3.3 Add test data

--- addons/inventory_editor/InventoryItem2D.gd
+++ addons/inventory_editor/InventoryItem2D.gd
@@ -35,32 +35,32 @@ func _on_body_entered(body: Node) -> void:
 
 func _on_body_exited(body: Node) -> void:
 	inside = false
-
-func _process(delta: float) -> void:
-	if item_db_uuid != item_put and item_put and not item_put.empty() and Engine.editor_hint:
-		if not get_tree().edited_scene_root.has_node(InventoryManagerName):
-			var root = get_tree().edited_scene_root
-			var manager = Node2D.new()
-			manager.name = InventoryManagerName
-			manager.set_script(load("res://addons/inventory_editor/InventoryManager.gd"))
-			root.add_child(manager)
-			manager.set_owner(get_tree().edited_scene_root)
-			var item_db = manager.get_item_db(item_put)
-			remove_old_childs(item_db)
-			if item_db and not item_db.scene.empty():
-				var scene = load(item_db.scene).instance()
-				scene.name = item_db.name
-				scene.set_meta("item_uuid", item_put)
-				if scene:
-					add_child(scene)
-					scene.set_owner(get_tree().edited_scene_root)
-			item_db_uuid = item_put
-			root.remove_child(manager)
-			manager.queue_free()
-
-func remove_old_childs(item_db: InventoryItem) -> void:
-	for child in get_children():
-		if child.has_meta("item_uuid") or item_db.name in child.name:
-			if child.get_meta("item_uuid") != item_put:
-				remove_child(child)
-				child.queue_free()
+#
+#func _process(delta: float) -> void:
+#	if item_db_uuid != item_put and item_put and not item_put.empty() and Engine.editor_hint:
+#		if not get_tree().edited_scene_root.has_node(InventoryManagerName):
+#			var root = get_tree().edited_scene_root
+#			var manager = Node2D.new()
+#			manager.name = InventoryManagerName
+#			manager.set_script(load("res://addons/inventory_editor/InventoryManager.gd"))
+#			root.add_child(manager)
+#			manager.set_owner(get_tree().edited_scene_root)
+#			var item_db = manager.get_item_db(item_put)
+#			remove_old_childs(item_db)
+#			if item_db and not item_db.scene.empty():
+#				var scene = load(item_db.scene).instance()
+#				scene.name = item_db.name
+#				scene.set_meta("item_uuid", item_put)
+#				if scene:
+#					add_child(scene)
+#					scene.set_owner(get_tree().edited_scene_root)
+#			item_db_uuid = item_put
+#			root.remove_child(manager)
+#			manager.queue_free()
+#
+#func remove_old_childs(item_db: InventoryItem) -> void:
+#	for child in get_children():
+#		if child.has_meta("item_uuid") or item_db.name in child.name:
+#			if child.get_meta("item_uuid") != item_put:
+#				remove_child(child)
+#				child.queue_free()

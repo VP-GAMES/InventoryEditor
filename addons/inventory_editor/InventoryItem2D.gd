364c78c7f97ad09cafa36ad4d2d0a230c3b2efc0
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-03-08 10:36:38 +0100

    V 0.5.3 Item2D support in editor script

--- addons/inventory_editor/InventoryItem2D.gd
+++ addons/inventory_editor/InventoryItem2D.gd
@@ -4,7 +4,7 @@ tool
 extends Area2D
 class_name Item2D
 
-var inside
+var inside 
 var _inventoryManager
 const InventoryManagerName = "InventoryManager"
 
@@ -12,10 +12,7 @@ export(String) var item_put # item_uuid
 export(String) var to_inventory # inventory_uuid
 export(int) var quantity = 1
 
-var item_db_uuid
-
 func _ready() -> void:
-	check_item_put_on_ready()
 	if get_tree().get_root().has_node(InventoryManagerName):
 		_inventoryManager = get_tree().get_root().get_node(InventoryManagerName)
 	if not is_connected("body_entered", self, "_on_body_entered"):
@@ -23,11 +20,13 @@ func _ready() -> void:
 	if not is_connected("body_exited", self, "_on_body_exited"):
 		assert(connect("body_exited", self, "_on_body_exited") == OK)
 
+"""
 func check_item_put_on_ready() -> void:
 	for child in get_children():
 		if child.has_meta("item_uuid"):
 			if child.get_meta("item_uuid") == item_put:
 				item_db_uuid = item_put
+"""
 
 func _on_body_entered(body: Node) -> void:
 	inside = true
@@ -35,32 +34,30 @@ func _on_body_entered(body: Node) -> void:
 
 func _on_body_exited(body: Node) -> void:
 	inside = false
-#
-#func _process(delta: float) -> void:
-#	if item_db_uuid != item_put and item_put and not item_put.empty() and Engine.editor_hint:
-#		if not get_tree().edited_scene_root.has_node(InventoryManagerName):
-#			var root = get_tree().edited_scene_root
-#			var manager = Node2D.new()
-#			manager.name = InventoryManagerName
-#			manager.set_script(load("res://addons/inventory_editor/InventoryManager.gd"))
-#			root.add_child(manager)
-#			manager.set_owner(get_tree().edited_scene_root)
-#			var item_db = manager.get_item_db(item_put)
-#			remove_old_childs(item_db)
-#			if item_db and not item_db.scene.empty():
-#				var scene = load(item_db.scene).instance()
-#				scene.name = item_db.name
-#				scene.set_meta("item_uuid", item_put)
-#				if scene:
-#					add_child(scene)
-#					scene.set_owner(get_tree().edited_scene_root)
-#			item_db_uuid = item_put
-#			root.remove_child(manager)
-#			manager.queue_free()
-#
-#func remove_old_childs(item_db: InventoryItem) -> void:
-#	for child in get_children():
-#		if child.has_meta("item_uuid") or item_db.name in child.name:
-#			if child.get_meta("item_uuid") != item_put:
-#				remove_child(child)
-#				child.queue_free()
+
+func _process(delta: float) -> void:
+	if Engine.editor_hint and item_put and not item_put.empty():
+		if not has_node("InventoryItemTexture_" + item_put):
+			_remove_old_childs()
+			if not get_tree().edited_scene_root.has_node(InventoryManagerName):
+				var root = get_tree().edited_scene_root
+				var manager = Node2D.new()
+				manager.name = InventoryManagerName
+				manager.set_script(load("res://addons/inventory_editor/InventoryManager.gd"))
+				root.add_child(manager)
+				manager.set_owner(get_tree().edited_scene_root)
+				var item_db = manager.get_item_db(item_put)
+				if item_db and not item_db.scene.empty():
+					var scene = load(item_db.scene).instance()
+					scene.name = "InventoryItemTexture_" + item_db.uuid
+					if scene:
+						add_child(scene)
+						scene.set_owner(get_tree().edited_scene_root)
+				root.remove_child(manager)
+				manager.queue_free()
+
+func _remove_old_childs() -> void:
+	for child in get_children():
+		if child.name.begins_with("InventoryItemTexture_"):
+			remove_child(child)
+			child.queue_free()

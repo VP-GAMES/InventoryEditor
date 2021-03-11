d1a8ec2590081032e88d2ee22fe18b228879e96f
Vladimir Petrenko <vladipetrenko@gmx.de>
2021-03-11 23:07:13 +0100

    V 0.6.7 InventoryEditor workin example

--- addons/inventory_editor/InventoryItem2D.gd
+++ addons/inventory_editor/InventoryItem2D.gd
@@ -11,10 +11,25 @@ const InventoryManagerName = "InventoryManager"
 export(String) var item_put # item_uuid 
 export(String) var to_inventory # inventory_uuid
 export(int) var quantity = 1
+export(bool) var remove_collected = true
 
 func _ready() -> void:
 	if get_tree().get_root().has_node(InventoryManagerName):
 		_inventoryManager = get_tree().get_root().get_node(InventoryManagerName)
+		var area = get_node("InventoryItem_" + item_put + "/Area2D")
+		if not area.is_connected("body_entered", self, "_on_body_entered"):
+			assert(area.connect("body_entered", self, "_on_body_entered") == OK)
+		if not area.is_connected("body_exited", self, "_on_body_exited"):
+			assert(area.connect("body_exited", self, "_on_body_exited") == OK)
+
+func _on_body_entered(body: Node) -> void:
+	inside = true
+	var remainder = _inventoryManager.add_item(to_inventory, item_put, quantity)
+	if remove_collected and remainder == 0:
+		queue_free() 
+
+func _on_body_exited(body: Node) -> void:
+	inside = false
 
 func _process(delta: float) -> void:
 	if Engine.editor_hint and item_put and not item_put.empty():
@@ -30,15 +45,8 @@ func _process(delta: float) -> void:
 				var item_db = manager.get_item_db(item_put)
 				if item_db and not item_db.scene.empty():
 					var scene = load(item_db.scene).instance()
-					scene.name = "InventoryItem_" + item_db.uuid
-					for child in scene.get_children():
-						if child is Area2D:
-							if not child.is_connected("body_entered", self, "_on_body_entered"):
-								assert(child.connect("body_entered", self, "_on_body_entered") == OK)
-							if not child.is_connected("body_exited", self, "_on_body_exited"):
-								assert(child.connect("body_exited", self, "_on_body_exited") == OK)
-							break
 					if scene:
+						scene.name = "InventoryItem_" + item_db.uuid
 						add_child(scene)
 						scene.set_owner(get_tree().edited_scene_root)
 				root.remove_child(manager)
@@ -49,11 +57,3 @@ func _remove_old_childs() -> void:
 		if child.name.begins_with("InventoryItem_"):
 			remove_child(child)
 			child.queue_free()
-
-
-func _on_body_entered(body: Node) -> void:
-	inside = true
-	_inventoryManager.add_item(to_inventory, item_put, quantity)
-
-func _on_body_exited(body: Node) -> void:
-	inside = false
--- addons/inventory_example/2d/Level1.gd
+++ addons/inventory_example/2d/Level1.gd
@@ -3,8 +3,17 @@ extends Node2D
 var inventoryManager
 
 onready var _error_ui = $CanvasError as CanvasLayer
+onready var _inventory = $CanvasInventory/Inventory
+onready var _inventory_button_ui = $CanvasButton/TextureButton as TextureButton
 
 func _ready() -> void:
 	if get_tree().get_root().has_node("InventoryManager"):
 		_error_ui.queue_free()
-		inventoryManager = get_tree().get_root().get_node("InventoryManager	")
+		inventoryManager = get_tree().get_root().get_node("InventoryManager")
+		_inventory_button_ui.connect("pressed", self, "_on_inventory_button_pressed")
+
+func _on_inventory_button_pressed() -> void:
+	if _inventory.visible:
+		_inventory.hide()
+	else:
+		_inventory.show()

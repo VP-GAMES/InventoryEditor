3691ee85f626a8b1f51dc9c9fc9766bb0f330907
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-03-22 16:51:50 +0100

    V 0.9.5 Add new inventory scene to example

--- addons/inventory_example/2d/Level.gd
+++ addons/inventory_example/2d/Level.gd
@@ -8,7 +8,9 @@ export(bool) var clear_inventory = true
 
 onready var _error_ui = $CanvasError as CanvasLayer
 onready var _inventory_any = $CanvasInventory/InventoryAny
+onready var _inventory_clothes = $CanvasInventory/InventoryClothes
 onready var _inventory_button_ui = $CanvasButton/TextureButton as TextureButton
+onready var _inventory_clothes_button_ui = $CanvasButton/TextureButtonClouthes as TextureButton
 
 func _ready() -> void:
 	if get_tree().get_root().has_node("InventoryManager"):
@@ -18,9 +20,17 @@ func _ready() -> void:
 		if clear_inventory:
 			inventoryManager.clear_inventory(_inventory_any.inventory)
 		_inventory_button_ui.connect("pressed", self, "_on_inventory_button_pressed")
+		_inventory_clothes_button_ui.connect("pressed", self, "_on_inventory_clothes_button_pressed")
+		
 
 func _on_inventory_button_pressed() -> void:
 	if _inventory_any.visible:
 		_inventory_any.hide()
 	else:
 		_inventory_any.show()
+
+func _on_inventory_clothes_button_pressed() -> void:
+	if _inventory_clothes.visible:
+		_inventory_clothes.hide()
+	else:
+		_inventory_clothes.show()

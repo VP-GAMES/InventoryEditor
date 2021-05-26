b56d01e07fcdd9d992b1b9f252a3b79a3a480c77
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-26 22:49:43 +0200

    V 1.4.0 Clear all error messages

--- addons/inventory_editor/ui/PopupDescription.gd
+++ addons/inventory_editor/ui/PopupDescription.gd
@@ -1,14 +1,15 @@
 # Example implementation for inventory item popup to demonstrate functionality of InventoryEditor : MIT License
 # @author Vladimir Petrenko
 extends Popup
-
+var _localization_manager_name = "LocalizationManager"
 var _localization_manager
 
 onready var _label = $Label as RichTextLabel
 
 func _ready() -> void:
 	if not _localization_manager:
-		_localization_manager = get_tree().get_root().get_node("LocalizationManager")
+		if get_tree().get_root().has_node(_localization_manager_name):
+			_localization_manager = get_tree().get_root().get_node(_localization_manager_name)
 
 func update_item_data(item: InventoryItem) -> void:
 	var text = item.description

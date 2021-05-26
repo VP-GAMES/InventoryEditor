4962eb1f351eeba938bfacc41771d4411a55bf9e
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-26 22:28:21 +0200

    V 1.4.0 Update popup

--- addons/inventory_editor/ui/PopupDescription.gd
+++ addons/inventory_editor/ui/PopupDescription.gd
@@ -2,7 +2,16 @@
 # @author Vladimir Petrenko
 extends Popup
 
+var localization_editor
+
 onready var _label = $Label as RichTextLabel
 
+func _ready() -> void:
+	if not localization_editor:
+		localization_editor = get_tree().get_root().find_node("LocalizationEditor", true, false)
+
 func update_item_data(item: InventoryItem) -> void:
-	_label.bbcode_text = item.description
+	var text = item.description
+	if localization_editor:
+		text = localization_editor.tr(text)
+	_label.bbcode_text = text

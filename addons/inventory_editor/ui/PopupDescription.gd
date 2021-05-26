ce9c7c51136602ca4922fa73efd1ade33f816363
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-26 22:41:09 +0200

    V 1.3.9 Update item description dropdown over localization manager

--- addons/inventory_editor/ui/PopupDescription.gd
+++ addons/inventory_editor/ui/PopupDescription.gd
@@ -2,16 +2,16 @@
 # @author Vladimir Petrenko
 extends Popup
 
-var localization_editor
+var _localization_manager
 
 onready var _label = $Label as RichTextLabel
 
 func _ready() -> void:
-	if not localization_editor:
-		localization_editor = get_tree().get_root().find_node("LocalizationEditor", true, false)
+	if not _localization_manager:
+		_localization_manager = get_tree().get_root().get_node("LocalizationManager")
 
 func update_item_data(item: InventoryItem) -> void:
 	var text = item.description
-	if localization_editor:
-		text = localization_editor.tr(text)
+	if _localization_manager:
+		text = _localization_manager.tr(text)
 	_label.bbcode_text = text
